7817a685ea8dc461e4f60e027c7b21d1404f1fa3
SamA1430 <samuel.arg14@gmail.com>
2023-02-11 20:37:02 -0500

    change JS

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ $(document).ready(function(){
         var height = $("#height").val();
         height = (height/100)**2;
         var bmi = weight/height;
-        bmi = Math.round(bmi)
-        $(".result").html("<h1>Your BMI is : "+ bmi +"</h1>");
+        bmi = Math.round(bmi);
+        if (isNaN(bmi)){
+            $(".result").html("<h1>Please enter valids values</h1>");
+        }
+        else{
+            $(".result").html("<h1>Your BMI is : "+ bmi +"</h1>");
+        }
     })
 });
